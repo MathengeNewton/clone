af8942b0c32556e3780d1076694ea6442347b732
Mathenge Newton <45616070+MathengeNewton@users.noreply.github.com>
2020-07-12 17:51:42 +0300

    message

---
+++ precache-manifest.d7102ff2c5dfd841342770ec19ddda4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca5d5d164f116ad1862e4cca17fa4c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62d92c1058597dacf74e",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "069bc577a2e6ed960e6a",
+    "url": "./static/css/main.d4f3f975.chunk.css"
+  },
+  {
+    "revision": "62d92c1058597dacf74e",
+    "url": "./static/js/2.843f5785.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.843f5785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069bc577a2e6ed960e6a",
+    "url": "./static/js/main.ca5d8f56.chunk.js"
+  },
+  {
+    "revision": "91e350d0512b04fab9b1",
+    "url": "./static/js/runtime-main.a39ad10a.js"
+  }
+]);
